02679b7e472a8b22c10474193ab70afa84ee2e9d
Kéwan B <mail@kewan.fr>
2025-04-02 13:54:56 +0200

    burger menu

--- src/js/script.js
+++ src/js/script.js
@@ -14,3 +14,12 @@ document.addEventListener("keydown", function (event) {
     }
   }
 })
+
+document.addEventListener("DOMContentLoaded", () => {
+  const burgerMenu = document.querySelector(".burger-menu");
+  const navLinks = document.querySelector(".nav-links");
+
+  burgerMenu.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+  });
+});
